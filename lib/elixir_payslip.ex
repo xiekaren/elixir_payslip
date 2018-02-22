f918305b55315cea04b8055c207801876be98bbd
Karen Xie <karen.xie@myob.com>
2018-02-23 08:26:57 +1300

    s  first commit

---
+++ lib/elixir_payslip.ex
@@ -0,0 +1,18 @@
+defmodule Payslip do
+  @moduledoc """
+  Documentation for Payslip.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Payslip.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_payslip_test.exs
@@ -0,0 +1,8 @@
+defmodule PayslipTest do
+  use ExUnit.Case
+  doctest Payslip
+
+  test "greets the world" do
+    assert Payslip.hello() == :world
+  end
+end
